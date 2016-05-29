29f52b41432796ea1458d057cd4e95f0cc85f0bf
gprakhar <prakhar.aaidu16@gmail.com>
2016-05-30 03:10:44 +0530

    deleted unnessary files, added new notebook for clustering, updated requirments.txt

--- tutorial-code_supervised-learning.py
+++
@@ -1,9 +0,0 @@
-from sklearn import tree
-
-features = [[140,1], [130,1], [150,0], [170,0]]
-labels = [0,0,1,1]
-classifier = tree.DecisionTreeClassifier()
-classifier = classifier.fit(features, labels)
-
-print classifier.predict([[160,0]])
-
